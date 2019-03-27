0eef9bbf659266794591d912bd09da419b0653e3
AbidemiT <abidemis01@gmail.com>
2019-03-27 13:11:59 +0100

    changed styling

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ const db = new Sequelize('codegig', 'root', '', {
   dialect: 'mysql',
   operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000,
+  // },
 });
 
 module.exports = db;
