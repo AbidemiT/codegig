b628b06660f68cd6797c0e772e2360e4ba2cfe18
AbidemiT <abidemis01@gmail.com>
2019-03-29 11:47:10 +0100

    latest commit

--- config/database.js
+++ config/database.js
@@ -11,6 +11,16 @@ const Sequelize = require('sequelize');
 //     idle: 10000,
 //   },
 // });
-const sequelize = new Sequelize('postgres://kbfwhpmwpzcxyj:da07969684b8a2a0968f5516144806b5a42c9d502809802a83f18c98c706dd05@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d2mvih7jco3mp4');
+// {
+//   "use_env_variable":"DB_CONNECTION_STRING",
+//   "dialect":"postgres",
+//   "ssl":true,
+//   "dialectOptions":{
+//      "ssl":{
+//         "require":true
+//      }
+//   }
+// }
+const db = new Sequelize('postgres://kbfwhpmwpzcxyj:da07969684b8a2a0968f5516144806b5a42c9d502809802a83f18c98c706dd05@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d2mvih7jco3mp4');
 
 module.exports = db;
