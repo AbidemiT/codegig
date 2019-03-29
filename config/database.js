913ca625e414a5f99d12aa40ceb3948be4d14f51
AbidemiT <abidemis01@gmail.com>
2019-03-29 12:44:04 +0100

    new commit

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
 const Sequelize = require('sequelize');
-// const db = new Sequelize('codegig', 'postgres', 'tasuedite1', {
-//   host: 'localhost',
-//   dialect: 'postgres',
-//   operatorsAliases: false,
+const db = new Sequelize('codegig', 'postgres', 'tasuedite1', {
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+  ssl: true,
+  operatorsAliases: false,
 
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// });
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 // {
 //   "use_env_variable":"DB_CONNECTION_STRING",
 //   "dialect":"postgres",
@@ -21,6 +23,7 @@ const Sequelize = require('sequelize');
 //      }
 //   }
 // }
-const db = new Sequelize('postgres://kbfwhpmwpzcxyj:da07969684b8a2a0968f5516144806b5a42c9d502809802a83f18c98c706dd05@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d2mvih7jco3mp4');
+// const db = new Sequelize('postgres://postgres:@127.0.0.1:5432/codegig?ssl=true');
+// const db = new Sequelize('postgres://kbfwhpmwpzcxyj:da07969684b8a2a0968f5516144806b5a42c9d502809802a83f18c98c706dd05@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d2mvih7jco3mp4?ssl=true');
 
 module.exports = db;
